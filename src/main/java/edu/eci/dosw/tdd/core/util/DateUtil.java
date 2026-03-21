e811b657520c3a348d48a4b985dd14fe35d3d9ab
CamiloWGod4564 <juan.cristancho-v@mail.escuelaing.edu.co>
2026-03-20 19:05:10 -0500

    Implementacion utiles

--- src/main/java/edu/eci/dosw/tdd/core/util/DateUtil.java
+++ src/main/java/edu/eci/dosw/tdd/core/util/DateUtil.java
@@ -1,4 +1,10 @@
 package edu.eci.dosw.tdd.core.util;
 
+import java.time.LocalDate;
+
 public class DateUtil {
+
+    public static LocalDate now() {
+        return LocalDate.now();
+    }
 }
--- src/main/java/edu/eci/dosw/tdd/core/util/IdGeneratorUtil.java
+++ src/main/java/edu/eci/dosw/tdd/core/util/IdGeneratorUtil.java
@@ -1,4 +1,20 @@
 package edu.eci.dosw.tdd.core.util;
 
 public class IdGeneratorUtil {
+
+    private static int bookCounter = 1;
+    private static int userCounter = 1;
+    private static int loanCounter = 1;
+
+    public static String generateBookId() {
+        return String.valueOf(bookCounter++);
+    }
+
+    public static String generateUserId() {
+        return String.valueOf(userCounter++);
+    }
+
+    public static String generateLoanId() {
+        return String.valueOf(loanCounter++);
+    }
 }
