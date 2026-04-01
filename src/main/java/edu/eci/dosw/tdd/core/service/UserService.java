7e74627c539ac69cdf80dc3dc94c8e0b4e30b28f
CamiloWGod4564 <juan.cristancho-v@mail.escuelaing.edu.co>
2026-03-31 22:05:57 -0500

    implementacion de services y dtos response

--- src/main/java/edu/eci/dosw/tdd/core/service/UserService.java
+++ src/main/java/edu/eci/dosw/tdd/core/service/UserService.java
@@ -1,4 +1,12 @@
 package edu.eci.dosw.tdd.core.service;
 
+import edu.eci.dosw.tdd.controller.dto.UserDTO;
+import edu.eci.dosw.tdd.controller.dto.UserResponseDTO;
+
+import java.util.List;
+
 public interface UserService {
+    public UserResponseDTO createUser(UserDTO userDTO);
+    public List<UserResponseDTO> getAllUsers();
+    public UserResponseDTO getUserById(String id);
 }
